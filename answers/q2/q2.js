91361803fa3ea3d295be6aac69749fa896827977
Adham226 <adhamnasserxiii@gmail.com>
2023-05-16 00:35:11 +0300

    modify js

--- answers/q2/q2.js
+++ answers/q2/q2.js
@@ -2,16 +2,16 @@ function maxProfit(jobs) {
     const len = jobs.length;
     jobs.sort((a, b) => a[1] - b[1]);
     const table = new Array(len + 1).fill(0);
-    for (let j = 1; j <= N; j++) {
+    for (let j = 1; j <= len; j++) {
         let i = j - 1;
         while (i >= 0 && jobs[i][1] > jobs[j - 1][0]) {
           i--;
         }
-        dp[j] = jobs[j - 1][2] + (i >= 0 ? dp[i + 1] : 0);
-        dp[j] = Math.max(dp[j], dp[j - 1]);
+        table[j] = jobs[j - 1][2] + (i >= 0 ? table[i + 1] : 0);
+        table[j] = Math.max(table[j], table[j - 1]);
       }
     
-      return dp[N];
+      return table[len];
 }
 const jobs = [
   [1, 6, 6],
